4d5a58b8d55f8568247cee1b477c3a7a251c7e1b
achrafjbr <developer@gmail.com>
2026-05-19 10:22:40 +0200

    install cors package for talkin to backend

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,30 @@
 const dotenv = require("dotenv");
-const ErrorMiddleware=require("./middlewares/errorHandler")
+const ErrorMiddleware = require("./middlewares/errorHandler");
 dotenv.config();
 const connectDB = require("./config/db");
 
 const express = require("express");
 const authRoutes = require("./routes/authRoutes");
-const projectRoutes = require("./routes/projectRoutes")
+const projectRoutes = require("./routes/projectRoutes");
 const investmentRoutes = require("./routes/investmentRoutes");
 const adminRoutes = require("./routes/adminRoutes");
 
-
-
 const app = express();
+app.use(cors());
+
 connectDB();
 app.use(express.json());
 
 app.use("/auth", authRoutes);
-app.use("/api/projects",projectRoutes)
-app.use("/api/investments", investmentRoutes); 
+app.use("/api/projects", projectRoutes);
+app.use("/api/investments", investmentRoutes);
 app.use("/api/admin", adminRoutes);
 
-app.use((err, req, res,next) => {
+app.use((err, req, res, next) => {
   const status = err.statusCode || 500;
-  res.status(status).json({ error: err.message || "Server Error" ,message :err});
+  res
+    .status(status)
+    .json({ error: err.message || "Server Error", message: err });
 });
 app.use(ErrorMiddleware);
 
@@ -31,5 +33,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
 });
-app.use(ErrorMiddleware)
+app.use(ErrorMiddleware);
 module.exports = app;
