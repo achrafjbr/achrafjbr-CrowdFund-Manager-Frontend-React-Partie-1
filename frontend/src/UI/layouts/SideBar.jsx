8cf148d3c0f7f847360215a13ce1bbcf0732202b
zinebsektaoui <zinebsektaoui1@gmail.com>
2026-05-19 15:54:07 +0100

    Adjusting the sidebar style

--- frontend/src/UI/layouts/SideBar.jsx
+++ frontend/src/UI/layouts/SideBar.jsx
@@ -6,45 +6,55 @@ import {
   Users,
 } from "lucide-react";
 import Navigator from "../components/Navigator";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 function SideBar() {
   return (
-    <div className="bg-black  w-[20%] p-1.5 py-6 space-y-8 min-h-screen">
-      <div className="text-white">LOGO</div>
+    <div className="bg-[#0B1020] w-[18%] p-1.5 py-6 space-y-70 min-h-screen">
+      <div className="space-y-6">
+        <div className="flex items-center gap-3 text-white mb-6">
+          <i className="fa-solid fa-chart-line text-purple-600 text-3xl"></i>
 
-      {/* <Link to={"/Login"} />
-      <Link to={"/Login"} /> */}
+          <h2 className="text-2xl font-bold">
+            Crowd<span className="text-purple-600">Funder</span>
+          </h2>
+        </div>
 
-      {/*  */}
-      <Navigator to="/">
-        <LayoutDashboard />
-        <span>Dashboard</span>
-      </Navigator>
+        {/* <Link to={"/Login"} />
+        <Link to={"/Login"} /> */}
 
-      {/*  */}
-      <Navigator to="projects">
-        <FolderGit2 />
-        <span>Projects</span>
-      </Navigator>
+        {/*  */}
+        <Navigator to="/">
+          <LayoutDashboard />
+          <span>Dashboard</span>
+        </Navigator>
 
-      {/*  */}
-      <Navigator to="investors">
-        <Users />
-        <span>Investors</span>
-      </Navigator>
+        {/*  */}
+        <Navigator to="projects">
+          <FolderGit2 />
+          <span>Projects</span>
+        </Navigator>
 
-      <h2 className="mt-20 font-extralight">ACCOUNT</h2>
+        {/*  */}
+        <Navigator to="investors">
+          <Users />
+          <span>Investors</span>
+        </Navigator>
+      </div>
 
-      {/*  */}
-      <Navigator to="profile">
-        <UserRound />
-        <span>Profile</span>
-      </Navigator>
+      {/* <h2 className="mt-20 font-extralight">ACCOUNT</h2> */}
+      <div className="space-y-6">
+        {/*  */}
+        <Navigator to="profile">
+          <UserRound />
+          <span>Profile</span>
+        </Navigator>
 
-      {/*  */}
-      <div className=" text-white cursor-pointer flex gap-2">
-        <LogOut />
-        <span>Logout</span>
+        {/*  */}
+        <div className=" text-white cursor-pointer flex gap-2">
+          <LogOut />
+          <span>Logout</span>
+        </div>
       </div>
     </div>
   );
