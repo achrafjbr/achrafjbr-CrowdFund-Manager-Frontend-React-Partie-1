fa3c7125020b49dc2f98600ec57a82fe00c3b173
zinebsektaoui <zinebsektaoui1@gmail.com>
2026-05-21 16:58:19 +0100

    getting the projects from api url

---
+++ frontend/src/services/projectService.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8080/api/projects/my-projects";
+
+export const getProjects = async () => {
+  const token = localStorage.getItem("token");
+
+  if (!token) {
+    throw new Error("No authentication token found");
+  }
+
+  const response = await axios.get(API_URL, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response.data;
+};
