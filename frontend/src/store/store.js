d0f553afa0d84714cc3b4419ca55609f34d5145c
zinebsektaoui <zinebsektaoui1@gmail.com>
2026-05-21 16:57:59 +0100

    adding projectReducder in the store

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import AuthenticationSlice from "../store/slices/authenticationSlice";
+import projectReducer from "./slices/projectSlice";
+
 export const store = configureStore({
   reducer: {
     authentication: AuthenticationSlice,
+    projects: projectReducer,
   },
 });
 export default store;
